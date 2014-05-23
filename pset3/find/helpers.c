f6663701bd0181d84cbd5b22d368c3ed3982a0ce
Logan Yu <yu.logan@gmail.com>
2014-05-23 17:50:50 -0400

    added bubblesort to helper.c

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -11,6 +11,8 @@
 
 #include "helpers.h"
 
+#include <stdio.h>
+
 /**
  * Returns true if value is in array of n values, else false.
  */
@@ -33,5 +35,30 @@ bool search(int value, int values[], int n)
 void sort(int values[], int n)
 {
     // TODO: implement an O(n^2) sorting algorithm
+    
+    bool sorted = false;
+    int placeholder;
+    
+    do
+    {
+        sorted = true;
+        for (int i = 0; i < n-1; i++)
+        {
+
+            if (values[i] > values[i+1])
+            {
+                sorted = false;
+                placeholder = values[i];
+                values[i] = values[j];
+                values[j] = values[i];
+            }
+        }
+    }while(!sorted);
+    
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", values[i]);
+    }
+    
     return;
 }
